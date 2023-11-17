aa361b093d25a96128b31c5a63cad902f04691ae
Mathias <mathias.hager1@web.de>
2023-11-17 22:18:02 +0100

    enemy behavior

---
+++ Hackathon/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private float speed = 3f;
+    private float rayDist = 2f;
+    private bool movingRight;
+    public Transform groundCollision;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundCheck = Physics2D.Raycast(groundCollision.position, Vector2.down, rayDist);
+
+
+        if (!groundCheck.collider)
+        {
+
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+
+        }
+    }
+}
