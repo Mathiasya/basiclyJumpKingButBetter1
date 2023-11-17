84e1990dc62d9a9e4a724a98a7d388abb6123102
Mathias <mathias.hager1@web.de>
2023-11-17 17:54:56 +0100

    health initial

---
+++ Hackathon/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    private float maxHealth = 100;
+    [SerializeField] private float currentHealth;
+   private float damageAmount = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getDamage() {
+        
+
+        currentHealth -= damageAmount;
+
+    }
+
+    
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        getDamage();
+        Debug.Log("collision");
+    }
+
+
+
+}
