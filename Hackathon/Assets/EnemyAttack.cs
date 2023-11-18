94de722d0174f1a8afa8f1b1e2321bb55895c330
Mathias <mathias.hager1@web.de>
2023-11-18 16:20:23 +0100

    Enemy Attack

---
+++ Hackathon/Assets/EnemyAttack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerHealth.instance.GetDamage(20);
+    }
+}
--- Hackathon/Assets/Scripts/EnemyBehavior.cs
+++ Hackathon/Assets/Scripts/EnemyBehavior.cs
@@ -8,16 +8,36 @@ public class EnemyBehavior : MonoBehaviour
     private float rayDistance = 2f;
     private bool movingRight;
     public Transform groundCollision;
+    [SerializeField] private Animator animator;
+    public bool isPlayerNear = false;
+    public Transform playerTransform;
+
+    private Quaternion right;
+    private Quaternion left;
+
+
+
+    private void Awake()
+    {
+        right = Quaternion.LookRotation(Vector3.forward);
+        left = Quaternion.LookRotation(Vector3.back);
+    }
+
 
     private void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        EnemyPatrol();
+    }
+
+    private void EnemyPatrol() {
+
         RaycastHit2D groundCheck = Physics2D.Raycast(groundCollision.position, Vector2.down, rayDistance);
 
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
 
         if (!groundCheck.collider)
         {
-
+            Debug.Log("!near and !ground");
             if (movingRight)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
@@ -30,5 +50,102 @@ public class EnemyBehavior : MonoBehaviour
             }
 
         }
+
+        if (isPlayerNear && groundCheck)
+        {
+            Debug.Log("near and ground");
+            float xPositionDifference = playerTransform.position.x - transform.position.x;
+
+            transform.rotation = xPositionDifference < 0 ? left : right;
+
+            isPlayerNear = false;
+
+        }
+
+
+        /*
+        RaycastHit2D groundCheck = Physics2D.Raycast(groundCollision.position, Vector2.down, rayDistance);
+
+
+        if (isPlayerNear && groundCheck)
+        {
+            Debug.Log("near and ground");
+            float xPositionDifference = playerTransform.position.x - transform.position.x;
+
+            transform.rotation = xPositionDifference < 0 ? left : right;
+
+            isPlayerNear = false;
+
+        }
+       
+
+        
+        else
+        {
+
+            Debug.Log("!near and ground");
+
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+
+
+            if (!groundCheck.collider)
+            {
+                Debug.Log("!near and !ground");
+                if (movingRight)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
+
+            }
+
+
+        }*/
+
+
     }
+
+    private void EnemyAttack() {
+
+        bool enemyAttack = animator.GetBool("attack");
+        bool isInTransition = animator.IsInTransition(0);
+
+
+        if (!enemyAttack) {
+
+           animator.SetBool("attack", true);
+
+        } else if (enemyAttack && !isInTransition) {
+            animator.SetBool("attack", false);
+        }
+        
+    }
+
+    
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        
+       CapsuleCollider2D capsuleCollider = GetComponentInChildren<CapsuleCollider2D>();
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+            EnemyAttack();
+            capsuleCollider.enabled = true;
+        } 
+    }
+
+
+
+
+
+
 }
