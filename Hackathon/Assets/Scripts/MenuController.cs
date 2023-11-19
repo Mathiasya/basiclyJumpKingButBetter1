e641c58e4147b85e6e55b55652b1433689765c45
Sebastian Bogensperger <sebastian.bogensperger@hotmail.com>
2023-11-19 01:15:35 +0100

    menu good but not finished

---
+++ Hackathon/Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Levels To Load")]
+    public string _newGameLevel;
+    private string levelToLoad;
+    
+
+    public void NewGameDialogYes()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_newGameLevel);       
+    }
+
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
